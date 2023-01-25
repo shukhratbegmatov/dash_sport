3065168fc5dfee9abf63356c10030dc1581c31da
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2023-01-25 05:48:55 +0500

    fix

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f94d40c40a49e478985be90a37d7c499.js"
+  "/precache-manifest.15c9364a0e37846f6e3df95f7fb23f6c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dash_sport"});
